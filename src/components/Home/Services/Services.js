1a17f671cc57536e44596f626363f4139c865809
MuktadirHassan <tamimmuktadir@gmail.com>
2020-12-17 19:12:02 +0600

    updated readme.md

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import Service from './Service/Service';
-import { DisappearedLoading } from 'react-loadingg';
+
 const Services = () => {
     const api = 'https://fast-depths-25443.herokuapp.com/getServices'
     const [services, setServices] = useState([]);
+    const [loading, setLoading] = useState(true);
     useEffect(()=>{
         fetch(api)
         .then(res => res.json())
         .then(data => setServices(data))
         .catch(err => console.log(err,'Error getting services'))
+        .finally(() => setLoading(false))
     },[])
     return (
         <section className="container my-5">
             <h1 className='text-center font-weight-bold py-4 mb-4'>Provide awesome <span className="text-secondary">services</span></h1>
             <div className="row justify-content-center align-items-center">
                 {
+                    loading ? <div>Loading</div> : (
                     services ? (
                         services.map(service => <Service key={service._id} service={service}></Service>)
-                    ) : ( <DisappearedLoading/> )
+                    ) : ( <div>No services</div> )
+                    )
                 }
             </div>
+            
         </section>
     );
 };
