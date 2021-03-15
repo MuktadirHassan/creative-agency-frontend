5655d71c69ea25375b8cc823e701df9f8066f543
= <tamimmuktadir@gmail.com>
2021-03-15 22:18:55 +0600

    updated getIdToken

--- src/authentication/AuthContext.js
+++ src/authentication/AuthContext.js
@@ -1,30 +1,24 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
-import { app, auth, googleProvider} from './firebaseInit';
-import { BoxLoading } from 'react-loadingg';
-
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { app, auth } from "./firebaseInit";
 
 const AuthContext = createContext();
 export function useAuth() {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 }
 const setUserToken = async () => {
-    const token = await app.auth().currentUser.getIdToken()
-    sessionStorage.setItem('authToken', token);
-}
+  const token = await app.auth().currentUser?.getIdToken();
+  sessionStorage.setItem("authToken", token);
+};
 
-export const AuthProvider = ({children}) => {
-    const [user, setUser] = useState()
-    useEffect(()=>{
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            setUser(user);
-            setUserToken();
-        });
-        return unsubscribe;
-    },[])
-    
-    return (
-        <AuthContext.Provider value={user}>
-            {children}
-        </AuthContext.Provider>
-    );
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setUserToken();
+    });
+    return unsubscribe;
+  }, []);
+
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
 };
